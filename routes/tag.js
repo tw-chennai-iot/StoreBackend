412ba171b3eee125e26203452aba2a8758a8f8ca
Venkateswari <venkates@thoughtworks.com>
2017-01-14 02:13:02 +0530

    fixed /tagId contract

--- routes/tag.js
+++ routes/tag.js
@@ -6,7 +6,7 @@ router.post('/', function (req, res) {
     tag.create((data) => res.json(data))
 });
 
-router.post('/:tagId/associate', function (req, res) {
+router.post('/:tagId', function (req, res) {
     tag.update(req.params.tagId, req.body.productId, (data) => res.json(data))
 });
 
