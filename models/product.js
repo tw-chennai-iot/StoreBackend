bca9406d736a0dea71c049bd79113fc47b0ff489
Venkateswari <venkates@thoughtworks.com>
2017-01-14 19:03:03 +0530

    create product with price

--- models/product.js
+++ models/product.js
@@ -2,10 +2,10 @@ var db = require('../db/db');
 var assert = require('assert');
 const uuidV4 = require('uuid/v4');
 
-var create = function (productName, callback) {
+var create = function (productName,price, callback) {
     db.execute((db) => {
         var collection = db.collection('products');
-        collection.insertOne({_id: uuidV4(), name: productName}, function (err, r) {
+        collection.insertOne({_id: uuidV4(), name: productName, price: price}, function (err, r) {
                 assert.equal(null, err);
                 console.log("product is created");
                 callback(r.ops[0])
--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var product = require('../models/product');
 
 router.post('/', function (req, res) {
-    product.create(req.body.name, (data) => res.json(data))
+    product.create(req.body.name,req.body.price, (data) => res.json(data))
 });
 
 module.exports = router;
